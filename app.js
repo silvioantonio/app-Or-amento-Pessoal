3fd00ca380d1132e1d4180d0069dc42d5a5c8a90
silvioantonio <oliveira_0000@hotmail.com>
2019-03-19 10:52:32 -0300

    Criaçao da funçao CadastrarDespesas e da classe para armazenar os dados

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
+class Despesas{
+    constructor(ano, mes, dia, tipo, descricao, valor){
+        this.ano = ano
+        this.dia = dia
+        this.mes = mes
+        this.descricao = descricao
+        this.valor = valor
+        this.tipo = tipo
+    }
+}
+
 function cadastrarDespesas(){
-    
+    let ano = document.getElementById('ano')
+    let mes = document.getElementById('mes')
+    let dia = document.getElementById('dia')
+    let tipo = document.getElementById('tipo')
+    let descricao = document.getElementById('descricao')
+    let valor = document.getElementById('valor')
+
+    let despesas = new Despesas(
+        ano.value,
+        mes.value,
+        dia.value,
+        tipo.value,
+        descricao.value,
+        valor.value
+    )
+        console.log(despesas)
 }
